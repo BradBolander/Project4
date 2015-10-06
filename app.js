4327b8de5a83b82355c56896b494281602d16722
BradBolander <bradbolander@unknownac293aa2ed82.att.net>
2015-10-05 20:29:37 -0500

    fix

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-var express = require('express');
+// var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
